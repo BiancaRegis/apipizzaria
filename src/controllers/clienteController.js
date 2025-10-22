f5c50d7abb12a01985b7c8df69f747f30abe3a6a
BiancaRegis <bianca0511rdiniz@gmail.com>
2025-10-22 17:16:55 -0300

    controle de erros

---
+++ src/controllers/clienteController.js
@@ -0,0 +1,93 @@
+//Usamos 'import * as' para agrupar todas as exportações do service.
+import * as clienteService from '../services/clienteService.js';
+import Joi from 'joi';
+
+//Usando 'export const'
+export const clienteCreateSchema = Joi.object({
+
+    cpf: Joi.string().length(11).required(),
+    nome: Joi.string().required().max(100),
+    endereco: Joi.string().required().max(100),
+    bairro: Joi.string().allow(''),
+    cidade: Joi.string().allow(''),
+    cep: Joi.string().required(),
+    telefone: Joi.string().required(),
+    email: Joi.string().email().required(),
+    senha: Joi.string().min(6).required(),
+});
+
+export const clienteUpdateSchema = Joi.object({
+    nome: Joi.string().max(100),
+    endereco: Joi.string().max(100),
+    bairro: Joi.string().allow(''),
+    cidade: Joi.string().allow(''),
+    cep: Joi.string(),
+    telefone: Joi.string(),
+    email: Joi.string().email(),
+    senha: Joi.string().min(6),
+}).min(1);
+
+export const listarClientes = async (req, res) => {
+    try {
+        const clientes = await clienteService.findAll();
+        res.status(200).json(clientes);
+    } catch (err) {
+        console.error(`erro ao buscar clientes:`, err);
+        res.status(500).json({ error: `erro interno do servidor` });
+    }
+};
+
+export const listarClienteCpf = async (req, res) => {
+    try {
+        const { cpf } = req.params;
+        const cliente = await clienteService.findByCpf(cpf);
+        if (!cliente) {
+            return res.status(400).json({ error: `cliente não encontrado` });
+        }
+        res.status(200).json(cliente);
+    } catch (err) {
+        console.error(`erro ao buscar cliente:`, err);
+        res.status(500).json({ error: `erro interno do servidor` });
+    }
+};
+
+export const adicionarCliente = async (req, res) => {
+    try {
+        const novoCliente = await clienteService.create(req.body);
+        res.status(201).json({ message: `cliente adicionado com sucesso`, data: novoCliente });
+    } catch (err) {
+        console.error(`error ao adicionar cliente:`, err);
+        if (err.code === `ER_DUP_ENTRY`) {
+            return res.status(409).json({ error: `CPF já cadastrado` });
+        }
+        res.status(500).json({ error: `erro ao adicionar cliente` });
+    }
+};
+
+export const atualizarCliente = async (req, res) => {
+    try {
+        const { cpf } = req.params;
+        const update = await clienteService.update(cpf, req.body);
+        if (!update) {
+            return res.status(404).json({ error: `cliente não encontrado` });
+        }
+        res.status(200).json({ message: `cliente atualizado com sucesso` });
+    } catch (err) {
+        console.error(`erro ao atualizar cliente:`, err);
+        res.status(500).json({ error: `erro ao atualizar cliente` });
+    }
+};
+
+export const deletarCliente = async (req, res) => {
+    try {
+        const { cpf } = req.params;
+        const deleted = await clienteService.remove(cpf);
+        if (!deleted) {
+            return res.status(404).json({ error: `cliente deletado com sucesso` });
+        }
+        res.status(200).json({ message: `cliente deletado com sucesso` });
+    } catch (err) {
+        console.error(`error ao deletar cliente:`, err);
+        res.status(500).json({ error: `error ao deletar cliente` });
+    }
+}; 
---
+++ src/middlewares/validate.js
@@ -0,0 +1,12 @@
+const validate = (schema) => (req, res, next) => {
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+    next();
+};
+
+//USANDO O 'export default' PARA EXPORTAR A FUNÇÃO PRINCIPAL DO MÓDULO.
+export default validate; 
+

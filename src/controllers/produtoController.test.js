6448e77e761d2268a97142e1a96be41e31c1714b
BiancaRegis <bianca0511rdiniz@gmail.com>
2025-12-03 17:22:48 -0300

    correção/adição de teste

--- src/controllers/produtoController.test.js
+++ src/controllers/produtoController.test.js
@@ -1,7 +1,11 @@
+// 1. importamos o schema que queremos testar
 import { produtoCreateSchema } from "./produtoController.js";
 
+//2. agrupamos nossos testes com 'describe'
 describe("testes para criar um novo produto", () => {
+    //3. teste 1: o 'caminho feliz'
     it("deve validar um produto com dados corretos", () => {
+        // preparação (arrange)
         const produtoCerto = {
             idProduto: "5",
             nome: "Pizza do Matheus",
@@ -10,14 +14,16 @@ describe("testes para criar um novo produto", () => {
             imagem: "calabresaprodutoespecial.jpn",
             valor: 5.50
         };
+        //execução (act)
        const {error} = produtoCreateSchema.validate(produtoCerto);
+       //verificação (assert)
+       //se 'error' for undefined (ou nulo), a validação passou.
        expect(error).toBeFalsy();
     });
 
     it("deve retornar verdade para rejeitar um campo sem nome", () => {
         const nomeCerto = {
             idProduto: "3",
-            nome: '',
             descricao: "pizza de brócolis",
             tipo: "sobremesa",
             imagem:'bróclois.png',
@@ -25,6 +31,8 @@ describe("testes para criar um novo produto", () => {
         };
         const{error} = produtoCreateSchema.validate(nomeCerto);
         expect(error).toBeTruthy();
+
+        expect(error.details[0].message).toBe('"nome" is required'); 
     });
 
     it("deve retornar verdade para rejeitar o campo de valor negativo", () => {
@@ -38,18 +46,34 @@ describe("testes para criar um novo produto", () => {
         };
         const{error} = produtoCreateSchema.validate(numeroCerto);
         expect(error).toBeTruthy();
+        expect(error.details[0].message).toBe('"valor" must be a positive number');
     });
 
-    // it("deve permitir um produto com imagem em branco", () => {
-    //       const imagemCerta = {
-    //         idProduto: "5",
-    //         nome: "Pizza do Matheus",
-    //         descricao: "O patrão ficou maluco",
-    //         tipo: "Pizza",
-    //         imagem: '',
-    //         valor: 5.50
-    //     };
-    //     const{error} = produtoCreateSchema.validate(imagemCerta);
-    //     expert(error).toBeTruthy();
-    // });
+    it("deve rejeitar um produto com valor que não é um número", () => {
+        const valorErrado = {
+            idProduto: "10",
+            nome: "pizza da leti",
+            descricao: "pizza da letileti",
+            tipo: "pizza",
+            imagem: "",
+            valor: "cinquentao"
+        };
+        const{error} = produtoCreateSchema.validate(valorErrado);
+        expect(error).toBeTruthy();
+        expect(error.details[0].message).toBe('"valor" must be a number');
+    });
+
+    it("deve permitir um produto com imagem em branco", () => {
+        //preparação (arrange)
+          const imagemCerta = {
+            idProduto: "5",
+            nome: "Pizza do Matheus",
+            descricao: "O patrão ficou maluco",
+            tipo: "Pizza",
+            imagem: "",
+            valor: 5.50
+        };
+        const{error} = produtoCreateSchema.validate(imagemCerta);
+        expect(error).toBeFalsy()
+    });
 });
